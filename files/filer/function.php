6ead1c2e9e814485cafb6b12c6c882dfc8a9776a
Morten Andersen <morten@hjemmesider.dk>
2022-10-05 11:37:29 +0200

    grid-auto-flow
    grid-auto-flow: column;

--- files/filer/function.php
+++ files/filer/function.php
@@ -3,7 +3,6 @@
 function fil_loop() {
 
 	echo '<div class="fil">';
-
         $file = get_field('Fil');
         if( $file ) {
             echo '<div class="fil-title"><a href="' . $file['url'] . '" target="_blank">' . get_the_title() . '</a></div>';
@@ -20,7 +19,7 @@ function fil_loop() {
             echo '<div class="info">' . $info . '</div>';
         }
             
-        edit_post_link( __( '+', 'skolehjemmesider-domain' ), '<div class="edit">', '</div>' );
+        edit_post_link( __( 'edit', 'skolehjemmesider-domain' ), '<div class="edit">', '</div>' );
 
     echo '</div>';
 
