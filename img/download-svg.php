e9d3adbf5f9eef7f84978da4296607ac17f4cc23
Morten Andersen <morten@hjemmesider.dk>
2022-12-07 11:36:09 +0100

    download
    icon change

--- img/download-svg.php
+++ img/download-svg.php
@@ -1,6 +1,6 @@
 <?php
 function download_icon() {
-return '<svg width="100%" height="100%" viewBox="0 0 70 70" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" xml:space="preserve" xmlns:serif="http://www.serif.com/" style="fill-rule:evenodd;clip-rule:evenodd;stroke-linejoin:round;stroke-miterlimit:2;">
+return '<svg viewBox="0 0 70 70" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" xml:space="preserve" xmlns:serif="http://www.serif.com/" style="fill-rule:evenodd;clip-rule:evenodd;stroke-linejoin:round;stroke-miterlimit:2;">
     <g id="Save-alt" transform="matrix(3.89365,0,1.39144e-19,3.89098,-11.751,-11.6881)">
         <rect x="0" y="0" width="24" height="24" style="fill:none;fill-rule:nonzero;"/>
         <path id="Save-alt__Shape" d="M19,12L19,19L5,19L5,12L3,12L3,19C3,20.1 3.9,21 5,21L19,21C20.1,21 21,20.1 21,19L21,12L19,12ZM13,12.67L15.59,10.09L17,11.5L12,16.5L7,11.5L8.41,10.09L11,12.67L11,3L13,3L13,12.67Z" style="fill-rule:nonzero;"/>
